da5561ecb5fcb4e933d576fb4dd6f941934c8212
guybedford <guybedford@gmail.com>
2013-04-24 17:25:18 -0700

    build working

--- hbs.js
+++ hbs.js
@@ -2,21 +2,23 @@ define(['amd-loader', 'handlebars'], function(amdLoader, Handlebars) {
   var includeHelpers = /{{@([a-zA-Z-0-9\.\/\~]+)/g;
   return amdLoader('hbs', 'hbs', function(name, source, req, callback, errback, config) {
     // replace internal requires with helper form
-    var sanitize = function(helperModule) {
-      return helperModule.replace('/', '-').replace('.', '').replace('~', '_');
+    var sanitize = function(name) {
+      return name.replace('/', '-').replace('.', '').replace('~', '_');
     }
     var helpers = [];
-    source = source.replace(includeHelpers, function (match, dep) {
+    source = source.replace(includeHelpers, function(match, dep) {
       helpers.push(dep);
       return '{{' + sanitize(dep);
     });
     // require the helpers
-    req(helpers, function() {
-      for (var i = 0; i < arguments.length; i++)
-        Handlebars.registerHelper(sanitize(helpers[i]), arguments[i]);
+    helpers.unshift('handlebars/handlebars.runtime');
 
-      // return the compiled template
-      callback(Handlebars.compile(source));
-    });
+    // return the compiled template
+    var output = "define(" + JSON.stringify(helpers) + ", function(Handlebars) {\n"
+      + "  Handlebars.registerHelper('" + sanitize(helpers[1]) + "', arguments[1]);\n"
+      + "  var t = Handlebars.template(" + Handlebars.precompile(source) + "); \n"
+      + "  return t;\n"
+      + "});"
+    callback(output);
   });
 });
