8892f00d3ec541eaf29f30c41865933de0962eb7
Eric Eastwood <madlittlemods@gmail.com>
2014-02-22 20:31:56 -0600

    Added Support for Partials; Updated Readme
    Adds support for partials (for non-built). Also updated handlebars and
    package

--- README.md
+++ README.md
@@ -1,11 +1,80 @@
-RequireJS Handlebars Plugin
-===
+# RequireJS Handlebars Plugin
 
-Simple handlebars templates.
+Simple [handlebars](http://handlebarsjs.com/) template support for [RequireJS](http://requirejs.org/)
+
+## Version:
+
+Handlebars: `1.3.0`
+hbs.js: `0.0.3`
+
+## Installation:
+```
+require.config({
+	paths: {
+		handlebars: 'handlebars',
+		'handlebars-runtime': 'handlebars.runtime',
+		hbs: 'hbs'
+	},
+	shim: {
+		handlebars: {
+			exports: 'Handlebars'
+		},
+		'handlebars-runtime': {
+			exports: 'Handlebars'
+		}
+	}
+});
+```
+
+## Requirements: 
+ - [RequireJS](http://requirejs.org/): *you probably already have this...*
+ - [Handlebars](http://handlebarsjs.com/)
+ - [AMD Loader](https://github.com/guybedford/amd-loader): A RequireJS plugin helper module
+
+
+## Usage:
+```
+require(['hbs!App/Template/body-template'], function (bodyTmpl) {
+	// Use whatever you would to render the template function
+	document.body.innerHTML = bodyTmpl({adjective: "favorite"});
+});
+```
+
+Path: `App/Template/body-template.hbs`
+```
+<div class="the better handlebars plugin">
+	This is my {{ adjective }} template.
+</div>
+```
+
+## Partials
+
+To have partials in your templates just use the syntax and provide a path to the partial
+
+Path: `App/Template/body-template.hbs`
+```
+<div class="the better handlebars plugin">
+	This is my {{ adjective }} template.
+
+	Now here is the partial: 
+
+	{{! the path can also be relative to the current template: }}
+	{{> ./coolPartial }}
+</div>
+```
+
+Path: `App/Template/coolPartial.hbs`
+```
+<div class="cool partial">
+	Coolest partial in town
+	{{! you can even have more partials in a partial }}
+</div>
+```
+
+## Helpers:
 
 Helpers can be loaded by RequireJS Module ID.
 
-eg:
 ```
 Fullname: {{@some/helper name}}
 ```
--- hbs.js
+++ hbs.js
@@ -1,6 +1,69 @@
-define(['amd-loader', './handlebars'], function(amdLoader, Handlebars) {
+// https://github.com/guybedford/hbs
+
+define(['amd-loader', 'handlebars'], function(amdLoader, Handlebars) {
+
+  // via: http://stackoverflow.com/a/17903018/796832
+  function getUniqueValuesInArray(a) {
+    return a.reduce(function(p, c) {
+      if (p.indexOf(c) < 0) p.push(c);
+        return p;
+    }, []);
+  };
+
+  // Joins path segments.  Preserves initial "/" and resolves ".." and "."
+  // Original from: https://gist.github.com/creationix/7435851
+  // * heavily modified by MLM
+  // Unit tests: http://jsfiddle.net/MadLittleMods/3L4J8/
+  function resolvePath( /* path segments */ ) {
+    // Split the inputs into a list of path commands.
+    var parts = [];
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      parts = parts.concat(arguments[i].split("/"));
+    }
+    // Interpret the path commands to get the new resolved path.
+    var newParts = [];
+    var stillDotDot = true; // Used in the first piece to determine if still using `../`. This preserves it on the first piece
+    for (i = 0, l = parts.length; i < l; i++) {
+      var part = parts[i];
+      // Remove leading and trailing slashes
+      if (!part) continue;
+      // Interpret "." to pop the last segment
+      // If the second to last part is a dot
+      // and If there is not a trailing slash on the previous piece
+      if (part === "." && i == l-2 && parts[i-1]) {
+        newParts.pop();
+        stillDotDot = false;
+      }
+      else if(part === ".") {
+        stillDotDot = false;
+        continue;
+      }
+      // Interpret ".." to pop the last segment
+      // If not stillDotDot.
+      else if (part === ".." && !stillDotDot) {
+        newParts.pop();
+      }
+      else if (part === "..") {
+        newParts.push(part);         
+      }
+      // Push new path segments.
+      else {
+        stillDotDot = false;
+        newParts.push(part);
+      }
+    }
+    // Preserve the initial slash if there was one.
+    if (parts[0] === "") newParts.unshift("");
+    // Turn back into a single string path.
+    return newParts.join("/") || (newParts.length ? "/" : ".");
+  }
+
+
+  
   var includeHelpers = /{{@([a-zA-Z-0-9\.\/\~]+)/g;
+  var includePartials = /{{> ([a-zA-Z-0-9\.\/\~]+)/g;
   return amdLoader('hbs', 'hbs', function(name, source, req, callback, errback, config) {
+
     // replace internal requires with helper form
     var sanitize = function(name) {
       return name.replace('/', '-').replace('.', '').replace('~', '_');
@@ -11,7 +74,18 @@ define(['amd-loader', './handlebars'], function(amdLoader, Handlebars) {
       return '{{' + sanitize(dep);
     });
     // require the helpers
-    helpers.unshift('hbs/handlebars.runtime');
+    helpers.unshift('handlebars-runtime');
+
+
+    // Grab all the partials
+    // Keep in mind that this does not rule out things commented out but it doesn't hurt anything
+    var partials = [];
+    source = source.replace(includePartials, function(match, dep) {
+      partials.push(dep);
+      return '{{>' + sanitize(dep);
+    });
+    partials = getUniqueValuesInArray(partials);
+
 
     // return the compiled template
     if (config.isBuild) {
@@ -25,13 +99,30 @@ define(['amd-loader', './handlebars'], function(amdLoader, Handlebars) {
     else {
       // require the helpers
       req(helpers, function() {
+        // Register the helpers
         for (var i = 0; i < arguments.length; i++)
           Handlebars.registerHelper(sanitize(helpers[i]), arguments[i]);
 
-        // return the compiled template
-        var compiled = Handlebars.compile(source);
-        callback(function(o) {
-          return compiled(o);
+
+        // Now make the path relative
+        var partialReferences = [partials.length];
+        for (var i in partials)
+        {
+          var pathToPartial = resolvePath(name, partials[i]);
+          partialReferences[i] = 'hbs!' + pathToPartial;
+        }
+
+        // Require the partials
+        req(partialReferences, function() {
+          // Register the partials
+          for (var i = 0; i < arguments.length; i++)
+            Handlebars.registerPartial(sanitize(partials[i]), arguments[i]);
+
+          // return the compiled template
+          var compiled = Handlebars.compile(source);
+          callback(function(o) {
+            return compiled(o);
+          });
         });
       });
     }
